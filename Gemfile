dad170ebdb6801d50e97f788878c179f7c5cd4fe
Dmitry A <mr.ads@yandex.ru>
2017-04-13 14:05:37 +0300

    Add .env and basic http auth

--- Gemfile
+++ Gemfile
@@ -33,6 +33,7 @@ gem 'awesome_print'
 gem 'activeadmin', github: 'activeadmin'
 gem 'inherited_resources'
 gem 'chartkick'
+gem 'dotenv-rails'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def authenticate_admin_user!
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'admin' && password == (ENV['ADMIN_PASSWORD'] || 'admin')
+    end
+  end
 end
